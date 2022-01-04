8892d825d7f0d749de3a770d90f59c6cc42be722
Ninomiya Kan <ninomiyakan20051011@gmail.com>
2022-01-04 23:26:48 +0900

    😔😔APIを無に戻しました。

--- api/app.py
+++ api/app.py
@@ -1,19 +1,11 @@
-from selenium import webdriver
-import chromedriver_binary
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from fastapi import FastAPI
 import time
 import datetime
-
+import requests
 
 # Setup
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-Chrome =  webdriver.Chrome(chrome_options=options)
-driver = Chrome
-
 app = FastAPI()
+webarchive_top = "https://web.archive.org/"
 # Setup end
 
 # api system
@@ -21,22 +13,6 @@ app = FastAPI()
 def do_save(p: str):
     url = p
     try:
-        driver.get("https://web.archive.org/")
-        time.sleep(1)
-        print("1")
-        do_input = driver.find_element(by=By.NAME, value="url_preload")
-
-        do_input.send_keys(url)
-        do_input.send_keys(Keys.RETURN)
-        print("2")
-
-        time.sleep(0.5)
-
-        submit_button = driver.find_element(by=By.CLASS_NAME, value="web-save-button")
-        submit_button.send_keys(Keys.RETURN)
-        print("3")
-        print("取得したサイトのURL:" + url)
-        print("success!")
         return({"status": "success", "url": url})
     except:
         print(url)
