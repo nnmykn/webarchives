4d983d382c6e35a07c0480b195799429f267c421
ninomiya <hi@kan.run>
2022-01-04 11:00:42 +0900

    api大体完成

--- api/app.py
+++ api/app.py
@@ -2,31 +2,43 @@ from selenium import webdriver
 import chromedriver_binary
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from fastapi import FastAPI
 import time
+import datetime
 
+
+# Setup
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-
-Chrome =  webdriver.Chrome()
+options.add_argument('--headless')
+Chrome =  webdriver.Chrome(chrome_options=options)
 driver = Chrome
 
-url = "https://kan.run/"
-# url = input("URLを入力してください: ")
+app = FastAPI()
+# Setup end
 
-driver.get("https://web.archive.org/")
-time.sleep(1)
-print("1")
-do_input = driver.find_element(by=By.NAME, value="url_preload")
+# api system
+@app.get("/save")
+def do_save(p: str):
+    url = p
+    
+    try:
+        driver.get("https://web.archive.org/")
+        time.sleep(1)
+        print("1")
+        do_input = driver.find_element(by=By.NAME, value="url_preload")
 
-do_input.send_keys(url)
-do_input.send_keys(Keys.RETURN)
-print("2")
+        do_input.send_keys(url)
+        do_input.send_keys(Keys.RETURN)
+        print("2")
 
-time.sleep(2)
+        time.sleep(0.5)
 
-submit_button = driver.find_element(by=By.CLASS_NAME, value="web-save-button")
-submit_button.send_keys(Keys.RETURN)
-print("3")
+        submit_button = driver.find_element(by=By.CLASS_NAME, value="web-save-button")
+        submit_button.send_keys(Keys.RETURN)
+        print("3")
+        print("success!")
+        return({"status": "success", "url": url})
+    except:
+        return({"status": "error"})
 
-time.sleep(5)
+# api system end
